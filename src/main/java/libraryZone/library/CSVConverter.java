4f0c12e38085f32f8ce673b350c4cebc7d98a2bf
Piyaporn <piyapornlahr@gmail.com>
2023-07-09 23:41:56 +0100

    Data structure changed from Hashmap to List of Book class

--- src/main/java/libraryZone/library/CSVConverter.java
+++ src/main/java/libraryZone/library/CSVConverter.java
@@ -16,55 +16,48 @@ public class CSVConverter {
             CSVReader csvReader = new CSVReader(new FileReader(file));
             List<String[]> data = csvReader.readAll();
 
-            HashMap<String, String> booksMap = new HashMap<>();
-            String[] header = data.get(0);
-//            System.out.println("header: " + Arrays.toString(header));
+            List<Book> books = new ArrayList<>();
+            String[] header = data.get(0); //[Number, Title, Author, Genre, SubGenre, Publisher]
 
             String[] dataRow = {};
             for (int i = 1; i < data.size(); i++) {
-                dataRow = data.get(i);
-//                System.out.println("dataRow: " + Arrays.toString(dataRow));
-            }
+                dataRow = data.get(i); // each book data
 
-            Book newBook = new Book();
-            String col = null;
-            String row = null;
-            for (int j = 0; j < dataRow.length; j++) {
+                Book newBook = new Book();
+                String col = null;
+                String row = null;
+                for (int j = 0; j < dataRow.length; j++) {
 
-                col = header[j].trim();
-                row = dataRow[j].trim();
+                    col = header[j].trim();
+                    row = dataRow[j].trim();
 
-                booksMap.put(col,row);
-
-
-                switch (col) {
-                    case "Number":
-                        newBook.setId(Long.parseLong(row));
-                        break;
-                    case "Title":
-                        newBook.setTitle(row);
-                        break;
-                    case "Author":
-                        newBook.setAuthor(row);
-                        break;
-                    case "Genre":
-                        newBook.setGenre(row);
-                        break;
-                    case "SubGenre":
-                        newBook.setSubgenre(row);
-                        break;
-                    case "Publisher":
-                        newBook.setPublisher(row);
-                        break;
+                    switch (col) {
+                        case "Number":
+                            newBook.setId(Long.parseLong(row));
+                            break;
+                        case "Title":
+                            newBook.setTitle(row);
+                            break;
+                        case "Author":
+                            newBook.setAuthor(row);
+                            break;
+                        case "Genre":
+                            newBook.setGenre(row);
+                            break;
+                        case "SubGenre":
+                            newBook.setSubgenre(row);
+                            break;
+                        case "Publisher":
+                            newBook.setPublisher(row);
+                            break;
+                    }
                 }
+                books.add(newBook);
+                //newBook.setCounter(0);
             }
-//            newBook.setCounter(0);
-            booksMap.put(col, row);
-
 
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            String jsonBook = gson.toJson(booksMap);
-
+            String jsonBook = gson.toJson(books);
 
             FileWriter fileWriter = new FileWriter("src/main/java/libraryZone/library/books_data.json");
             fileWriter.write(jsonBook);
